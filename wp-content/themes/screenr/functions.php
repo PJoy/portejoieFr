fe537f6dce37f468712bd361ee18298e5c4508b8
root <root@portejoieFr>
2017-01-13 09:16:33 +0100

    duplicator

--- wp-content/themes/screenr/functions.php
+++ wp-content/themes/screenr/functions.php
@@ -334,3 +334,16 @@ require get_template_directory() . '/inc/class-slider.php';
 require get_template_directory() . '/inc/dashboard.php';
 
 //require_once( trailingslashit( get_template_directory() ) . 'trt-customizer-pro/example-1/class-customize.php' );
+
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90254387-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<?php } 
